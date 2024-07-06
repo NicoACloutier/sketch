95f4e2232e82895f546115b9195aa6d005bb7e1b
Nicolas Antonio Cloutier <nicocloutier1@gmail.com>
2024-07-06 15:46:17 -0500

    complete buddy algorithm

--- aggregate.py
+++ aggregate.py
@@ -23,6 +23,18 @@ def best_buddies(stroke_i: np.ndarray, stroke_j: np.ndarray) -> list[tuple[int, 
     indeces = [closest(stroke_j, point) for point in stroke_i]
     return [(index, i) for (i, index) in enumerate(indeces) if closest(stroke_i, stroke_j[index]) == i]
 
+def buddy_point(buddy: tuple[int, int], stroke_i: np.ndarray, stroke_j: np.ndarray):
+    """
+    Find the mean point coordinates between two best buddy points.
+    Arguments:
+        `buddy: tuple[int, int]`: the index of the buddy points in the first and second stroke, respectively.
+        `stroke_i: np.ndarray`: the first stroke.
+        `stroke_j: np.ndarray`: the second stroke.
+    Returns:
+        `np.ndarray`: the average point.
+    """
+    return np.mean(np.array([stroke_i[buddy[0]], stroke_j[buddy[1]]), axis=0)
+
 def aggregate(stroke_i: np.ndarray, stroke_j: np.ndarray) -> np.ndarray:
     """
     Aggregate two strokes to a common one using best buddies.
@@ -33,6 +45,9 @@ def aggregate(stroke_i: np.ndarray, stroke_j: np.ndarray) -> np.ndarray:
         `np.ndarray`: aggregate stroke.
     """
     buddies = best_buddies(stroke_i, stroke_j)
-    i_max, i_min = max(buddies, key=(lambda x: x[0])), min(buddies, key=(lambda x: x[0]))
-    j_max, j_min = max(buddies, key=(lambda x: x[0])), min(buddies, key=(lambda x: x[0]))
-    raise NotImplementedError()
+    buddy_coords = np.array([buddy_point(buddy, stroke_i, stroke_j) for buddy in buddies])
+    i_max, i_min = max(buddies, key=(lambda x: x[0]))[0], min(buddies, key=(lambda x: x[0]))[0]
+    j_max, j_min = max(buddies, key=(lambda x: x[1]))[1], min(buddies, key=(lambda x: x[1]))[1]
+    stroke_i = np.concat(stroke_i[:i_min], stroke_i[i_max+1:]) 
+    stroke_j = np.concat(stroke_j[:j_min], stroke_j[j_max+1:])
+    return np.concatenate(stroke_i, buddy_coords, stroke_j) if i_min < j_min else np.concatenate(stroke_j, buddy_coords, stroke_i)
