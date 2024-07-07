a6c19d5fe5bc2205913ca59412bc1f28e63777e9
Nicolas Antonio Cloutier <nicocloutier1@gmail.com>
2024-07-07 17:19:05 -0500

    fix angular distance function

--- angular_compatibility.py
+++ angular_compatibility.py
@@ -60,26 +60,26 @@ def average_distance(aggregate_stroke: np.ndarray, stroke_i: np.ndarray, stroke_
     assert aggregate_stroke.shape[1] == 3   
    
     distance = sum(np.linalg.norm(curve_map(point, stroke_i) - curve_map(point, stroke_j)) for point in aggregate_stroke)
-    return distance / np.linalg.norm(aggregate_stroke)  
+    return distance / len(aggregate_stroke)  
 
-def angular_distance(stroke_i: np.ndarray, stroke_j: np.ndarray) -> float:
+def angular_distance(stroke_i: np.ndarray, aggregate_stroke: np.ndarray) -> float:
     """
     Angular distance between two strokes ($D_a$ in Liu et al. 2018).
     Arguments:
         `stroke_i: np.ndarray`: stroke of shape `(n_points, 3)`.
-        `stroke_j: np.ndarray`: stroke of shape `(n_points, 3)`.
+        `aggregate_stroke: np.ndarray`: aggregate stroke of shape `(n_points, 3)`.
     Returns:
         `float`: Angular distance `phi` between strokes.    
     """
     
-    ## `stroke_i` and `stroke_j` should be 2D ndarrays
+    ## should be 2D ndarrays
     assert len(stroke_i.shape) == 2
-    assert len(stroke_j.shape) == 2
+    assert len(aggregate_stroke.shape) == 2
     ## the second dimension should be 3 for 3D
     assert stroke_i.shape[1] == 3
-    assert stroke_j.shape[1] == 3   
- 
-    return (1 / np.linalg.norm(stroke_i - stroke_j))
+    assert aggregate_stroke.shape[1] == 3   
+
+    return sum(angular_difference(point, stroke_i, aggregate_stroke) for point in stroke_i) / len(aggregate_stroke)
 
 def angular_compatibility(stroke_i: np.ndarray, stroke_j: np.ndarray, aggregate_stroke: np.ndarray) -> float:
     """
