6495c0e1a186a202004d198bc5f8f9226180e25f
Nicolas Antonio Cloutier <nicocloutier1@gmail.com>
2024-07-10 22:35:13 -0500

    add visualized test

---
+++ test.py
@@ -0,0 +1,28 @@
+import aggregate
+import numpy as np
+import trimesh
+
+def main():
+    
+    ## strait line, hardcoded
+    s_i_one = np.array( [ [0, 0, 0], [1, 1, 0], [2, 2.5, 0] ] )
+    s_j_one = np.array( [ [2, 1.5, 0], [3, 3, 0], [4, 4, 0] ] )
+    s_a_one = aggregate.aggregate(s_i_one, s_j_one)
+    lines = [trimesh.load_path(line) for line in [s_i_one, s_j_one, s_a_one]]
+    for line in lines:
+        line.colors = [np.append(np.random.random(3), 1)]
+    trimesh.Scene(lines[:-1]).show()
+    trimesh.Scene(lines).show()
+
+    ## curved line, perfect function 
+    s_i_two = np.array( [ [x/1000, (x/1000)**1.1, 0] for x in range(70) ] ) 
+    s_j_two = np.array( [ [x/1000, x/1000-.02, 0] for x in range(30, 100) ] ) 
+    s_a_two = aggregate.aggregate(s_i_two, s_j_two)
+    lines = [trimesh.load_path(line) for line in [s_i_two, s_j_two, s_a_two]]
+    for line in lines:
+        line.colors = [np.append(np.random.random(3), 1)]
+    trimesh.Scene(lines[:-1]).show()
+    trimesh.Scene(lines).show()
+
+if __name__ == "__main__":
+    main()
